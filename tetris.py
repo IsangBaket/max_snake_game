54bd248521aff0db3c0dcc89af35610d3cb2db1d
IsangBaket? <max.bias@myyahoo.com>
2025-06-26 22:39:21 +0800

    added grids

--- tetris.py
+++ tetris.py
@@ -5,8 +5,15 @@ class Tetris:
     def __init__(self, app):
         self.app = app
 
+    def draw_grid(self):
+        for x in range(field_w):
+            for y in range(field_h):
+                pg.draw.rect(self.app.screeb, 'black',
+                             (x * title_Size, y * title_Size, title_Size, title_Size), 1)
+
+
     def update(self):
         pass
 
     def draw(self):
-        pass
+        self.draw_grid()
