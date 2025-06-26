0b93583711f5591dd12b2762e7dfccf1606e4456
IsangBaket? <max.bias@myyahoo.com>
2025-06-26 22:28:53 +0800

    created structure for tetris app

---
+++ main.py
@@ -0,0 +1,34 @@
+from settings import *
+from tetris import Tetris
+import sys
+
+class App:
+    def __init__(self):
+        pg.init()
+        pg.display.set_caption('Tetris')
+        self.screen = pg.display.set_mode(field_res)
+        self.clock = pg.time.Clock()
+        self.tetris - Tetris(self)
+
+    def update(self):
+        self.clock.tick(fps)
+
+    def draw(self):
+        self.screen.fill(color=field_color)
+        pg.display.flip()
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.quit or (event.type == pg. KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+    
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
---
+++ settings.py
@@ -0,0 +1,8 @@
+import pygame as pg
+
+fps = 60
+field_color = (48, 39, 32)
+
+title_Size = 50
+field_size = field_w, field_h = 10, 20
+field_res = field_w* title_Size, field_h* title_Size
