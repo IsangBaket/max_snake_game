a5668901a9fe7e415323b8efc2502c4d2a1cb253
IsangBaket? <max.bias@myyahoo.com>
2025-06-26 22:54:03 +0800

    imported tetromino to tetris ---implemented another abstract layer for tetromino

--- tetris.py
+++ tetris.py
@@ -1,19 +1,21 @@
 from settings import *
 import math
+from tetromino import Tetromino
 
 class Tetris:
     def __init__(self, app):
         self.app = app
+        self.tetromino = Tetromino(self)
 
     def draw_grid(self):
         for x in range(field_w):
             for y in range(field_h):
-                pg.draw.rect(self.app.screeb, 'black',
+                pg.draw.rect(self.app.screen, 'black',
                              (x * title_Size, y * title_Size, title_Size, title_Size), 1)
 
 
     def update(self):
-        pass
+        self.tetromino.update()
 
     def draw(self):
         self.draw_grid()
